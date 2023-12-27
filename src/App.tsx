82dd44389b55f07ee29f4f5166da49848f5fd269
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-12-27 21:09:47 +0330

    update loading page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import Router from "./router";
 // ** providers
 import { ToastProvider } from "@providers";
 
+// ** components
+import { Head } from "@components";
+
 const App = () => (
   <>
+    <Head title="Willo"/>
     <ToastProvider>
       <Router />
     </ToastProvider>
--- src/libs/components/LoadingPage/index.tsx
+++ src/libs/components/LoadingPage/index.tsx
@@ -8,7 +8,7 @@ export const LoadingPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center flex-col gap-3">
       <LogoIcon className="h-32 w-32" />
-      <LoadingSpinner className="w-16 h-16" />
+      <LoadingSpinner className="!w-12 !h-12" />
     </div>
   );
 };
